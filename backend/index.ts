4d3a68f60c434cdb5ac33531b0b50cf61dc41c80
irunislaw <84100482+irunislaw@users.noreply.github.com>
2023-05-26 21:56:21 +0200

    Revert "Frontend slides"

--- backend/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { productRouter } from "./routers/productRouter";
-import { t } from "./utils/trpc";
-const app = express();
-
-export const appRouter = t.router({
-    products: productRouter
-});
--- backend/zod/envSchema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const envSchema = z.object({
-    DATABASE_URL: z.string().url(),
-    JWT_SECRET: z.string().min(64),
-    JWT_REFRESH_SECRET: z.string().min(64)
-});
