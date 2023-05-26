721f720223afc12aa69aa91010e070c8276b0ca2
irunislaw <84100482+irunislaw@users.noreply.github.com>
2023-05-26 18:28:34 +0200

    impleneted auth

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,8 @@
 import express from "express";
-import { createContext, t } from "./utils/trpc";
 import { productRouter } from "./routers/productRouter";
+import { t } from "./utils/trpc";
 const app = express();
 
 export const appRouter = t.router({
     products: productRouter
 });
-
